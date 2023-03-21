2ce13030ff1e84f71a1dbabf4aa25788952a51c8
Aditya Soni <soniaditya20032002@gmail.com>
2023-03-22 00:30:17 +0530

    Added fith page of HOME

--- src/Components/Home/home_fifth_page/home_fifth_page.jsx
+++ src/Components/Home/home_fifth_page/home_fifth_page.jsx
@@ -10,7 +10,9 @@ function HomeFifthPage() {
                 <h3>Health Information for All, Everywhere</h3>
                 <p>Health Umbrella Foundation is a movement to collect and summarize the testimonials of common people regarding various treatments and therapies. The organization aims to systematically arrange and verify feedback from different sources, including direct interviews,websites, magazines, and newspapers. This data can provide valuable insights to doctors and healthcare professionals regarding the effectiveness of treatments in real world scenarios</p>
             </div>
-            <SearchBar />
+            <div className="search">
+                <SearchBar />
+            </div>
             <div className="hfifthp_lower">
                 <img src="./Images/Group 69.png" alt="" />
             </div>

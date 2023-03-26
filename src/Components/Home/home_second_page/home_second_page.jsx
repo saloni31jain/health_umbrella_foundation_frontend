1811f152e6b20931693db31db16aa3ca282b6d1e
Aditya Soni <soniaditya20032002@gmail.com>
2023-03-26 11:17:15 +0530

    implemented the read more button logic

--- src/Components/Home/home_second_page/home_second_page.jsx
+++ src/Components/Home/home_second_page/home_second_page.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./home_second_page.css";
+import { useState } from "react";
 
 function HomeSecondPage(){
+
+    let more="Read More";
+    let less="Read less"
+    const [collapse,setCollapse]=useState(false);
+
+    function clickHandler(){
+        // console.log("Button is clicked");
+        setCollapse(prev => !prev);
+    }
+
     return(
         <div className="hsp_outer">
             <div className="hsp_outer_mid">
@@ -13,10 +24,13 @@ function HomeSecondPage(){
                     <h1 className="hsp_outer_mid_right_heading">Our Mission</h1>
                     <span className="hsp_outer_mid_right_text">The Internet contains 
                     huge amounts of data in the form of
-                     testimonials of patients in the form of ebooks, websites, 
-                     YouTube videos, articles which are scattered, unverified and 
-                     unorganised.</span>
-                     <button className="hsp_outer_mid_right_button">Read More</button>
+                    testimonials of patients in the form of ebooks, websites, 
+                    YouTube videos, articles which are scattered, unverified and 
+                    unorganised.</span>
+                    <br/>
+                    
+                    <span className={`${collapse?"hsp_outer_mid_right_text":"hsp_outer_mid_right_text_collapse"}`}>Our goal is to bridge the gap between patients and healthcare professionals by fostering a community of support and education, promoting a healthy lifestyle, and advocating for equitable access to quality healthcare for all.</span>
+                     <button className="hsp_outer_mid_right_button" onClick={clickHandler}>{collapse?'Read less':'Read More'}</button>
                 </div>
             </div>
         </div>

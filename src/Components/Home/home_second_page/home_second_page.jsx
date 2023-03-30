651946f695abd359ea313f1bc1f72111b4f36290
Aditya Soni <soniaditya20032002@gmail.com>
2023-03-30 17:09:31 +0530

    implemented the read more button, decrease the size of videos in the video section, implemented the seach bar logic and made some small changes to increase the responsiveness

--- src/Components/Home/home_second_page/home_second_page.jsx
+++ src/Components/Home/home_second_page/home_second_page.jsx
@@ -26,7 +26,7 @@ function HomeSecondPage() {
                         testimonials of patients in the form of ebooks, websites,
                         YouTube videos, articles which are scattered, unverified and
                         unorganised.</span>
-                    <br />
+                    
                     
                     <span className={collapse ? "hsp_outer_mid_right_text" : "hsp_outer_mid_right_text_collapse"}>Our goal is to bridge the gap between patients and healthcare professionals by fostering a community of support and education, promoting a healthy lifestyle, and advocating for equitable access to quality healthcare for all.</span>
                     <button className="hsp_outer_mid_right_button" onClick={clickHandler}>{collapse ? 'Read Less' : 'Read More'}</button>

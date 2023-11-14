c579202f401561518baaf4fbb41a54c804ab95aa
Dhruv Bandi <dhruvbandi03@gmail.com>
2023-11-14 13:01:26 +0530

    accupressure page done

--- src/Components/Accupressure/accupressure.jsx
+++ src/Components/Accupressure/accupressure.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './accupressure.css';
+import axios from 'axios';
 
 const Accupressure = () => {
+  const [data,setData]=useState({
+    pathy:'',
+    text:'',
+    informationSource:[]
+});
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://backend.healthumbrella.org:8000/disease/migraine/acupressure');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  fetchData();
+},[]);
+
+
   return (
     <div className='accupressure-main'>
       <div className='accupressure-nav'>&lt; Migraine / Accupressure</div>
@@ -9,29 +29,29 @@ const Accupressure = () => {
         <img src="./Images/green_ring_tl.png" className="greenring-tl" />
         <h1 className="accupressure-heading">ACUPRESSURE</h1>
         <h3 className="accupressure-subheading">THERAPY</h3>
-        <div className="accupressure-content">Discover life-altering testimonials showcasing our effective migraine solutions. From various sources like YouTube, Quora, social media, direct feedback, doctors' clinics, books, and articles, these authentic accounts offer valuable insights. Gain inspiration and validation through the transformative journeys of migraine sufferers who found relief with us. Experience the positive impact of our solutions firsthand. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum ipsa labore dolor! Nobis, aliquid ex nam sequi quod delectus corporis, dolor officia optio obcaecati ullam eveniet quas alias error blanditiis Lorem ipsum dolor sit amet consectetur adipisicing elit. At odio, voluptas, ratione illo unde libero tempore corrupti recusandae ipsam sunt alias quo quis sapiente dolores deserunt doloribus consequatur voluptatibus. Nemo.</div>
+        <div className="accupressure-content">{data.text}</div>
         <div className="accupressure-cards">
-            <div className="item 1">
+            <div className="item">
               <img src="./Images/man.png" alt="" />
               <div className="accupressure-cards-desc">Direct Cases</div>
             </div>
-            <div className="item 2">
+            <div className="item">
               <img src="./Images/youtube-logo.png" alt="" />
               <div className="accupressure-cards-desc">Youtube</div>
             </div>
-            <div className="item 3">
+            <div className="item">
               <img src="./Images/globe-icon.png" alt="" />
               <div className="accupressure-cards-desc">Website</div>
             </div>
-            <div className="item 4">
+            <div className="item">
               <img src="./Images/socialmedia-women.png" alt="" />
               <div className="accupressure-cards-desc">SocialMedia</div>
             </div>
-            <div className="item 5">
+            <div className="item">
               <img src="./Images/books.png" alt="" />
               <div className="accupressure-cards-desc">Books/Book Chapters</div>
             </div>
-            <div className="item 6">
+            <div className="item">
               <img src="./Images/articles-search.png" alt="" />
               <div className="accupressure-cards-desc">Articles</div>
             </div>
